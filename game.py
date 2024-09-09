7eece4f09d3e3f27194edfcc704d74662dd9d1db
Alejandro Kong <115722702+alexkm174x8@users.noreply.github.com>
2024-09-09 12:39:16 -0600

    Finished rectangle

--- game.py
+++ game.py
@@ -43,6 +43,22 @@ def circle(start, end):
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    width = end.x - start.x  
+    height = end.y - start.y 
+
+    forward(width)
+    left(90)
+    forward(height)
+    left(90)
+    forward(width)
+    left(90)
+    forward(height)
+
+    end_fill()
     pass  # TODO
 
 
